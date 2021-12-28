ba344d4765f8243eb5f2f003f7a73e9958d9535a
jmk <jmkolata@gmail.com>
2021-12-27 22:22:01 -0500

    using UserService to add/remove components based on admin role

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { UserService } from './../services/user.service';
 import {Component, OnInit} from '@angular/core';
 import {Course} from '../model/course';
 import {Observable, of} from 'rxjs';
@@ -20,7 +21,8 @@ export class HomeComponent implements OnInit {
 
     constructor(
       private router: Router,
-      private coursesService: CoursesService) { }
+      private coursesService: CoursesService,
+      public user: UserService) { }
 
     ngOnInit() {
       this.reloadCourses();
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import { Injectable } from "@angular/core";
 import { map } from 'rxjs/operators';
 import { Router } from '@angular/router';
+import { UserRoles } from '../model/user-roles';
 
 
 @Injectable({providedIn: 'root'})
@@ -11,20 +12,31 @@ export class UserService {
     isLoggedIn$: Observable<boolean>;
     isLoggedOut$: Observable<boolean>;
     pictureUrl$: Observable<string>;
+    roles$: Observable<UserRoles>;
 
     constructor(
         private afAuth: AngularFireAuth,
         private router: Router) {
-        this.isLoggedIn$ = afAuth.authState.pipe(
-                                //this evaluates to true if this object
-                                //is present
-                                map(user => !!user)
-                            );
+        this.isLoggedIn$ = afAuth.authState
+            .pipe(
+                //this evaluates to true if this object
+                //is present
+                map(user => !!user)
+            );
         this.isLoggedOut$ = this.isLoggedIn$.pipe(map(loggedIn => !loggedIn));
 
-        this.pictureUrl$ = afAuth.authState.pipe(
-                                map(user => user ? user.photoURL : null)
-                            );
+        this.pictureUrl$ = afAuth.authState
+            .pipe(
+                map(user => user ? user.photoURL : null)
+            );
+        
+        this.roles$ = afAuth.idTokenResult
+            .pipe(
+                //if the token doesn't contain any claims,
+                //then by default we create some for them and set
+                //the admin userrole to false
+                map(token => <any>token?.claims ?? {admin:false})
+            );
     }
 
     logout() {
